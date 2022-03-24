36d1da75468a06aacb94ec8621cc61aff7b2342f
Gabriele Almeida <naogabe@gmail.com>
2022-03-24 18:35:31 -0300

    refactor: Comentando código

--- app/Console/Commands/ReadExternalApi.php
+++ app/Console/Commands/ReadExternalApi.php
@@ -26,13 +26,23 @@ class ReadExternalApi extends Command
 
          $apiUrl = 'https://api.adviceslip.com/advice';
 
+         /* Acesso o método getCurl, da classe adviceAPIService através do construtor
+         ** Decodifica uma string json e converte pro php, como um array associativo
+         ** Pega a informação que eu quero do array/matriz
+         */
+
          $advice_request = $this->adviceAPIService->getCurl($apiUrl);
          $adviceDecode = json_decode($advice_request, true);
 
          $dailyAdvice = $adviceDecode['slip']['advice'];
 
          var_dump($dailyAdvice);
-         //a partir daqui é salvar numa tabela advice esse campo $dailyAdvice
+
+         /* A partir daqui é salvar numa tabela advice esse campo $dailyAdvice
+         ** Cria uma nova instancia do model
+         ** Atribui o valor de dailyadvice a essa nova instancia
+         ** Salva no banco
+         */
 
          $advice = new Advice();
 
--- app/Services/AdviceApiService.php
+++ app/Services/AdviceApiService.php
@@ -4,18 +4,40 @@ namespace App\Services;
 
 class AdviceAPIService {
 
+    /* O cURL é uma ferramenta para criar requisições e obter conteúdo remoto. 
+    Ele existe como ferramenta de linha de comando, e também como biblioteca, 
+    a libcurl, que o PHP incorpora e expõe por meio das funções curl_. */
+
+    /* Gera uma requisição http em $url
+    A função curl_setopt define todos os parâmetros da requisição */
     public function getCurl( $url )
-    {
+    {   
+        // Inicializa sessão curl
         $ch = curl_init();
 
+
+        /* Definindo parâmetros da requisição, como ela deve ser feita*/
+        // Define que a requisição vai ser gerada em $url
         curl_setopt($ch, CURLOPT_URL,$url);
+
+        // Exibe a resposta da requisição normalmente, ignorando códigos de erro
         curl_setopt($ch, CURLOPT_FAILONERROR,1);
+
+        // Siga redirecionamentos http
+        // As vezes ao solicitar uma URL, podemos ser direcionados para outra URL
+        // Se essa opção estiver como true, vamos seguir para essa outra URL, seguindo esse fluxo
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+
+        // foi definido como true, a função curl_exec (que dispara a requisição) 
+        // irá retornar o conteúdo recuperado da URL
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 
-        $data = curl_exec($ch);          
+        // Executa a sessão cURL, dispara a requisição
+        $data = curl_exec($ch);  
+        // Fecha sessão cURL 
         curl_close($ch);
 
+        // Retorna os dados de resposta da requisição
         return $data;
     }
 
